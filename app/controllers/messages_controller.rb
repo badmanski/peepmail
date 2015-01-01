bb012969b75bae684d380eaa09446c234c9c019b
Stanislav Gorski <stanislav.gorski@gmail.com>
2015-01-01 19:17:14 +0200

    Remove variable assignment in messages#create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    message = Message.create(message_params)
+    Message.create(message_params)
     redirect_to root_path
   end
 
